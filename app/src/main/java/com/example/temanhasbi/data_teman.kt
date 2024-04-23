a0d957c6f3071c9b69b71bb79cd69a2a5e868a66
Abdullahhasbi26 <hasbiabdullah279@gmail.com>
2024-04-23 23:05:29 +0700

    PPB CRUD

---
+++ app/src/main/java/com/example/temanhasbi/data_teman.kt
@@ -0,0 +1,19 @@
+package com.example.temanhasbi
+
+class data_teman {
+
+    var nama: String? = null
+    var alamat: String? = null
+    var no_hp : String? = null
+    var key : String? = null
+
+
+    constructor(){}
+
+
+    constructor(nama: String?,alamat: String?, no_hp: String?){
+        this.nama = nama
+        this.alamat = alamat
+        this.no_hp = no_hp
+    }
+}
